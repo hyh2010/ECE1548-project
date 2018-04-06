299422479d74974eadf48bcb801f374a09332b27
hyh2010 <yihuan.huang@gmail.com>
2018-04-06 16:32:03 -0400

    Oops I forgot to test the sample code!

--- sim_network.py
+++ sim_network.py
@@ -7,7 +7,7 @@ interarrival_time = 2
 number_of_packets = 5
 
 env = simpy.Environment()
-server = Queuesim.ServerConstServiceTime(env, service_time)
+server = Queuesim.ServerConstServiceTime(env, capacity=1, service_time=service_time)
 source = Queuesim.TrafficSourceConstInterarrival(server, interarrival_time)
 source.add_traffic_generator_process(number_of_packets)
 env.run()
